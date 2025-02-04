36b10732c822eecc9bc1a823d0e79f63ad1d31ad
SuaDeDua <sokvai123hn@gmail.com>
2025-02-04 11:18:24 +0700

    customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="test" size="lg">click me</Button>
+    <div className=''>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
     </div>
   );
 }
